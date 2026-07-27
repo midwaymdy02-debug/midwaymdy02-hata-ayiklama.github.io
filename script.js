b19a6f2050f36e9e547eb53aa581a288a03cac4f
𝐦𝐢𝐝𝐰𝐚𝐲 <midwaymdy02@gmail.com>
2026-07-27 19:16:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const text = "Python Developer • Minecraft Developer";
+const texts = [
+    "Python Developer",
+    "Minecraft Developer",
+    "Web Developer",
+    "Discord Bot Developer"
+];
 
-let i = 0;
+let textIndex = 0;
+let charIndex = 0;
+let deleting = false;
 
-function type(){
+const typing = document.getElementById("typing");
 
-    if(i < text.length){
+function animate() {
 
-        document.getElementById("typing").innerHTML += text.charAt(i);
+    const current = texts[textIndex];
 
-        i++;
-
-        setTimeout(type,80);
-
+    if (!deleting) {
+        typing.textContent = current.substring(0, charIndex++);
+    } else {
+        typing.textContent = current.substring(0, charIndex--);
     }
 
+    let speed = deleting ? 50 : 100;
+
+    if (!deleting && charIndex === current.length + 1) {
+        deleting = true;
+        speed = 1500;
+    }
+
+    if (deleting && charIndex === 0) {
+        deleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+    }
+
+    setTimeout(animate, speed);
 }
 
-type();
+animate();
