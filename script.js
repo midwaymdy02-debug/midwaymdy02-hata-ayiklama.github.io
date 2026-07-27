f71e6d90a8cab2a588c54b3614b508160a54844b
𝐦𝐢𝐝𝐰𝐚𝐲 <midwaymdy02@gmail.com>
2026-07-27 19:10:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const texts = [
+    "Python Developer",
+    "Minecraft Developer",
+    "Web Developer",
+    "Midway"
+];
 
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+    if (count === texts.length) {
+        count = 0;
+    }
+
+    currentText = texts[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+
+        setTimeout(() => {
+
+            index = 0;
+            count++;
+
+            setTimeout(type, 500);
+
+        }, 1500);
+
+    } else {
+
+        setTimeout(type, 100);
+
+    }
+
+})();
